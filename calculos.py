0849833f4cbe0b8dc3c08720473ab792b4f5300a
Bruno Alves <josebrunosouzaalves@gmail.com>
2025-10-06 14:49:46 -0300

    fix: função para_notacao_cientifica

--- calculos.py
+++ calculos.py
@@ -1,5 +1,5 @@
 import math
-def para_notacao_cientifica(numero, formato_projeto=False):
+def para_notacao_cientifica(numero):
     # Trata o caso especial do zero
     if numero == 0:
         return 0.0, 0
@@ -8,12 +8,14 @@ def para_notacao_cientifica(numero, formato_projeto=False):
     # math.floor arredonda para baixo, nos dando a potência de 10 correta
     expoente = math.floor(math.log10(abs(numero)))
 
-    # Se for o formato do projeto (0.dddd...), o expoente é uma unidade maior
-    if formato_projeto:
-        expoente += 1
+
+    
 
     # Calcula a mantissa dividindo o número pela potência de 10
     mantissa = numero / (10**expoente)
+    if  (mantissa >= 9.99998):
+            mantissa = mantissa / 10.0
+            expoente = expoente + 1
     
     #usar print assim:
     #mantissa,expoente = para_notacao_cientifica(valor_exato,False)
@@ -154,23 +156,22 @@ def CancelamentoSub(x,y,n_digitos,metodo):
     #para saber o valor exato basta diminuir normal (x-y)
     
 
-x_teste = 0.56786
-y_teste = 0
-vezes = 10
+x_teste = 0.76545
+y_teste = 0.76541
+vezes = 0
 ndigitos = 4
 metodo = 1
-op = 1
+op = 2
 
 
-""" 
-TESTE CANCELAMENTO SUB
+
 print(f"aproximado arredondado: {aproximacao(x_teste,ndigitos,metodo)}")
 print("-"*20)
 valorexato = x_teste - y_teste
 valoraproximado = CancelamentoSub(x_teste,y_teste,ndigitos,metodo)
 erroabsoluto = erroAbsoluto(valorexato,valoraproximado)
 errorelativo = erroRelativo(valorexato,valoraproximado)
-"""
+
 
 
 """"
@@ -179,11 +180,15 @@ valoraproximado = propErroAprox(x_teste,vezes,ndigitos,metodo,op)
 
 erroabsoluto = erroAbsoluto(valorexato,valoraproximado)
 errorelativo = erroRelativo(valorexato,valoraproximado)
-
-print(f"valor exato {valorexato}")
-print(f"valor aproximado{valoraproximado}")
-print(f"comparação de erros EA:{erroabsoluto}")
-print(f"comparação de erros ER:{errorelativo}") 
-mantissa,expoente = para_notacao_cientifica(x_teste,False)
-print(f"Notação Científica Padrão: {mantissa:.1f} x 10^{expoente}") 
 """
+
+
+print(f"valor exato: {valorexato}")
+print(f"valor aproximado: {valoraproximado}")
+print(f"comparação de erros EA: {erroabsoluto}")
+print(f"comparação de erros ER: {errorelativo}") 
+print("+"*20)
+mantissa,expoente = para_notacao_cientifica(valorexato)
+print(f"Notação Científica Padrão valor aprox: {mantissa:.1f} x 10^{expoente}") 
+mantissa,expoente = para_notacao_cientifica(x_teste)
+print(f"Notação Científica Padrão valor aprox: {mantissa:.1f} x 10^{expoente}") 
